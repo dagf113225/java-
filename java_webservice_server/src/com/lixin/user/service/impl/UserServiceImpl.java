6b3e9e4d7c49fca22314346c38a703f4a64b6361
dagf113225 <512444862@qq.com>
2018-12-21 11:33:28 +0800

    javawebservice服务发布1

--- java_webservice_server/src/com/lixin/user/service/impl/UserServiceImpl.java
+++ java_webservice_server/src/com/lixin/user/service/impl/UserServiceImpl.java
@@ -4,31 +4,41 @@ import java.util.List;
 
 import javax.jws.WebService;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import com.lixin.model.Role;
 import com.lixin.user.dao.DB;
 import com.lixin.user.service.interfaces.IUserService;
 
-@WebService(portName="userservice",
-serviceName="UserServiceImpl",
-targetNamespace="http://thzm.com/wsdl",
-endpointInterface="com.lixin.user.service.interfaces.IUserService")
-public class UserServiceImpl   implements IUserService{
+@WebService(portName = "userservice", serviceName = "UserServiceImpl", targetNamespace = "http://thzm.com/wsdl", endpointInterface = "com.lixin.user.service.interfaces.IUserService")
+public class UserServiceImpl implements IUserService {
 
 	@Override
 	public String queryRoleData() {
 		// TODO Auto-generated method stub
 		System.out.println("UserServiceImpl  is queryRoleData  start...  ");
-		
-		
-		DB  db  = new DB();
-		
-		List<Role>   lists=db.queryRoleData();
-		
-		System.out.println("--->"+lists.size());
-		
-		
-		return "南京科技职业学院";
+
+		DB db = new DB();
+
+		List<Role> lists = db.queryRoleData();
+
+		System.out.println("--->" + lists.size());
+
+		// webservice发布的数据应该是各个平台和语言统一能够解析的数据格式:
+		// json [{},{},{}]
+
+		JSONArray array = new JSONArray();
+		for (Role role : lists) {
+
+			JSONObject obj = new JSONObject();
+			obj.put("id", role.getRid());
+			obj.put("rname", role.getRname());
+			array.add(obj);
+		}
+		System.out.println("JSON-->" + array.toString());
+
+		return array.toString();
 	}
-	
 
 }
